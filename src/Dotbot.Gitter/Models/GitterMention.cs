b6c592a5ea0bd3d8be8a3b0d239b8d27383d7365
Patrik Svensson <patrik@patriksvensson.se>
2017-05-15 12:06:21 +0200

    Removed Gitter adapter for now.
    Closes #15

--- src/Dotbot.Gitter/Models/GitterMention.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dotbot.Gitter.Models
-{
-    internal sealed class GitterMention
-    {
-        public string UserId { get; set; }
-        public string ScreenName { get; set; }
-    }
-}
